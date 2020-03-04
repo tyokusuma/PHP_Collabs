146bed2deca7e1c1b083afbfed0f0cd24f4f0ca6
Fismatami <avilafismatami@gmail.com>
2020-03-04 11:11:02 +0700

    proses update

---
+++ proses_update.php
@@ -0,0 +1,21 @@
+<?php
+
+	include "koneksi.php";
+
+	$id = $_POST['id'];
+	$nama = $_POST['nama'];
+	$alamat = $_POST['alamat'];
+	$jenis_kelamin = $_POST['jenis_kelamin'];
+	$pendidikan = $_POST['pendidikan'];
+	$divisi = $_POST['divisi'];
+
+	$sql = "update karyawan set nama='$nama', alamat='$alamat', jenis_kelamin='$jenis_kelamin',pendidikan='$pendidikan' where id='$id'";
+	$query = mysqli_query($koneksi,$sql);
+
+	if ($sql) {
+		echo "berhasil update";
+	} else {
+		echo "gagal update";
+	}
+
+?>
